6e75899c38112348960495d4f3c34c1a3077dea6
ViramC420 <c.viramontes248@gmail.com>
2023-12-04 21:42:54 -0800

    function.php was using sql, changed to nosql

--- functions.php
+++ functions.php
@@ -1,56 +1,65 @@
 <?php
 // Connect to database
-$host     = "localhost";
-$username = "root";
-$password = "";
-$database = "food_recipe";
+$mongoClient = new MongoDB\Client("mongodb: ");
+$database = $mongoClient->food_recipe;
+$collection = $database->user;
 
-$connect = mysqli_connect($host, $username, $password, $database);
-
-// check connection
-if (!$connect) {
-die("connection error: " . mysqli_connect_error());
+// Check if connection is successful
+try {
+if ($mongoClient) {
+echo "Connected to MongoDB successfully!";
+// Now proceed with other operations using $collection
+} else {
+echo "Failed to connect to MongoDB.";
+}
+} catch (MongoDB\Driver\Exception\Exception $e) {
+echo "MongoDB Exception: " . $e->getMessage();
 }
 
 
 // Registration Function
-
-function registrasi($data)
+function registrasi($data, $collection)
 {
-global $connect;
+$username = strtolower($data["username"]);
+$password = $data["password"];
+$password2 = $data["password2"];
 
-$username = strtolower(stripslashes($data["username"]));
-$password = mysqli_real_escape_string($connect, $data["password"]);
-$password2 = mysqli_real_escape_string($connect, $data["password2"]);
-
-// check if username exist
-$result = mysqli_query($connect, "SELECT username
-                                    FROM user 
-                                    WHERE username = '$username'
-                                    ");
-
-if (mysqli_fetch_assoc($result)) {
-    echo "<script>
-            alert('Username already exists');
-            </script>";
+// Check if username exists
+$result = $collection->findOne(['username' => $username]);
+if (!empty($result)) {
+    echo "<script>alert('Username already exists');</script>";
     return false;
 }
 
-// check password confirmation
+// Check password confirmation
 if ($password !== $password2) {
-    echo "<script>
-            alert('Confirmed password is incorrect');
-            </script>";
+    echo "<script>alert('Confirmed password is incorrect');</script>";
     return false;
 }
 
-// encrypt password
+// Encrypt password
 $password = password_hash($password, PASSWORD_DEFAULT);
 
-// add user to db
-mysqli_query($connect, "INSERT INTO user VALUES('', '$username', '$password')");
+// Add user to MongoDB
+$userDocument = [
+    'username' => $username,
+    'password' => $password
+];
+$insertResult = $collection->insertOne($userDocument);
 
-return mysqli_affected_rows($connect);
+return $insertResult->getInsertedCount();
 }
 
-// Registration Function
+// Usage
+$data = [
+'username' => 'example_user',
+'password' => 'example_password',
+'password2' => 'example_password'
+];
+
+$result = registrasi($data, $collection);
+if ($result) {
+echo "User registered successfully!";
+} else {
+echo "Failed to register user.";
+}
